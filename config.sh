031d859a9fa1a949aa26e599c1b44bb2c49b07c9
ZXY <7743182+xidianzxy@user.noreply.gitee.com>
2021-10-12 12:57:46 +0000

    conf

--- config.sh
+++ config.sh
@@ -1,3 +1,14 @@
 export INTEL_ROOT=/opt/intel/oneapi
 export INTEL_VERSION=2021.4.0
 export MPI_ROOT=/home/zhuxingyu/devtools/mpi/mpich3
+
+export INTEL_ROOT=/opt/intel/oneapi
+export INTEL_VERSION=2021.4.0
+export MPI_ROOT=/home/zhuxingyu/devtools/mpi/mpich3
+export DEFS=-DINTEL_MKL
+
+TOP_PATH=`pwd`
+LIB_PATH=$TOP_PATH/lib
+
+export LDLIBS=-L$LIB_PATH
+export LDFLAGS=-Wl,-rpath=$LIB_PATH -Wl,-rpath=${INTEL_ROOT}/compiler/${INTEL_VERSION}/linux/compiler/lib/intel64_lin/
